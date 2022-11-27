82f84b3d16cc5a9856d757769cc69dc0b5a08a75
Maksim Pisartsev <Maxwell88@yandex.ru>
2022-11-27 11:04:56 +0300

    Feat: add app, connect to mongoDB, create schemas

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const { PORT = 3000 } = process.env;
+
+mongoose.connect('mongodb://localhost:27017/mestodb');
+
+app.listen(3000, err =>
+	err ? console.log(`${err.name}, ${err.message}`) : console.log(`Server OK, port ${PORT}`)
+);
---
+++ models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true,
+		minlength: 2,
+		maxlength: 30,
+		validate: {
+			validator(v) {
+				return v >= 2 && v <= 30;
+			},
+			message: 'Name must be more than 2 and less than 30 characters',
+		},
+	},
+	about: {
+		type: String,
+		required: true,
+		minlength: 2,
+		maxlength: 30,
+		validate: {
+			validator(v) {
+				return v >= 2 && v <= 30;
+			},
+			message: 'Description must be more than 2 and less than 30 characters',
+		},
+	},
+	avatar: {
+		type: String,
+		required: true,
+	},
+});
+
+module.exports = mongoose.model('user', userSchema);
