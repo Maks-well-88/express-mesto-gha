dd1495a8858c1c5e40e2b9366e8adf8326ee20e0
Maksim Pisartsev <Maxwell88@yandex.ru>
2022-11-27 11:28:51 +0300

    Feat: add editorconfig

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const { PORT = 3000 } = process.env;
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect("mongodb://localhost:27017/mestodb");
 
-app.listen(3000, err =>
-	err ? console.log(`${err.name}, ${err.message}`) : console.log(`Server OK, port ${PORT}`)
+app.listen(3000, (err) =>
+  err
+    ? console.log(`${err.name}, ${err.message}`)
+    : console.log(`Server OK, port ${PORT}`)
 );
--- models/card.js
+++ models/card.js
@@ -1,35 +1,35 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const cardSchema = new mongoose.Schema({
-	name: {
-		type: String,
-		required: true,
-		minlength: 2,
-		maxlength: 30,
-		validate: {
-			validator(v) {
-				return v >= 2 && v <= 30;
-			},
-			message: 'Name must be more than 2 and less than 30 characters',
-		},
-	},
-	link: {
-		type: String,
-		required: true,
-	},
-	owner: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'user',
-		required: true,
-	},
-	likes: {
-		type: [{ type: ObjectId, ref: 'user' }], // тут нужно точно проверить правильность типа
-		default: [],
-	},
-	createdAt: {
-		type: Date,
-		default: Date.now,
-	},
+  name: {
+    type: String,
+    required: true,
+    minlength: 2,
+    maxlength: 30,
+    validate: {
+      validator(v) {
+        return v >= 2 && v <= 30;
+      },
+      message: "Name must be more than 2 and less than 30 characters",
+    },
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  likes: {
+    type: [{ type: ObjectId, ref: "user" }], // тут нужно точно проверить правильность типа
+    default: [],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-module.exports = mongoose.model('card', cardSchema);
+module.exports = mongoose.model("card", cardSchema);
