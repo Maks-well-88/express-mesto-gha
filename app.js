f181481efe51ffeb439cb83214edef6d54ccf951
Maksim Pisartsev <Maxwell88@yandex.ru>
2022-11-27 12:13:30 +0300

    Fix: add body-parser

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
 const app = express();
 const { PORT = 3000 } = process.env;
-
 mongoose.connect("mongodb://localhost:27017/mestodb");
+
+app.use(bodyParser.json());
 
 app.listen(3000, (err) =>
   err
