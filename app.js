ef2cc481071b2ae7d7f36d31a562073a3398af7a
Maksim Pisartsev <Maxwell88@yandex.ru>
2022-12-01 14:36:52 +0300

    Fix: change get to patch

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-app.get('*', (req, res) => {
+app.patch('*', (req, res) => {
   res.status(404).send({ message: 'This page does not exist' });
 });
 
