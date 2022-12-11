203a3106b4ada364ac8cfce4a7339c69c29a9475
Maksim Pisartsev <Maxwell88@yandex.ru>
2022-12-11 18:45:25 +0300

    Feat: create route and controller for get info about User

--- controllers/users.js
+++ controllers/users.js
@@ -32,6 +32,22 @@ const getUser = async (req, res) => {
   }
 };
 
+const getMe = async (req, res) => {
+  try {
+    const user = await userModel.findById(req.user._id);
+    if (!user) {
+      return res.status(constants.NOT_FOUND).send({ message: constants.NOT_FOUND_MESSAGE });
+    }
+    return res.status(constants.OK).send(user);
+  } catch (error) {
+    console.error(`${error.name}: ${error.message}`);
+    if (error.name === 'CastError') {
+      return res.status(constants.BAD_REQUEST).send({ message: constants.CAST_ERROR_MESSAGE });
+    }
+    return res.status(constants.SERVER_ERROR).send({ message: constants.SERVER_ERROR_MESSAGE });
+  }
+};
+
 const createUser = async (req, res) => {
   try {
     const hash = await bcrypt.hash(req.body.password, 10);
@@ -77,7 +93,7 @@ const login = async (req, res) => {
   } catch (error) {
     console.error(`${error.name}: ${error.message}`);
     if (error.name === 'TypeError') {
-      return res.status(constants.UNAUTHORIZED).send({ message: constants.NO_ACCESS_MESSAGE });
+      return res.status(constants.BAD_REQUEST).send({ message: constants.NO_ACCESS_MESSAGE });
     }
     return res.status(constants.SERVER_ERROR).send({ message: constants.SERVER_ERROR_MESSAGE });
   }
@@ -137,6 +153,7 @@ const updateAvatar = async (req, res) => {
 
 module.exports = {
   getUser,
+  getMe,
   getUsers,
   createUser,
   updateProfile,
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
 const {
   getUser,
+  getMe,
   getUsers,
   updateProfile,
   updateAvatar,
 } = require('../controllers/users');
 
 router.get('/', getUsers);
+router.get('/me', getMe);
 router.get('/:userId', getUser);
 router.patch('/me', updateProfile);
 router.patch('/me/avatar', updateAvatar);
